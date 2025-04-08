fb626e34a4fa951164463c61922e8d59500eaea2
Phoschi <phoschimere@gmail.com>
2025-04-08 14:43:41 +0200

    Ajout du dashboard et d'un header

--- src/main/java/org/exemple/spoonmate/Finance.java
+++ src/main/java/org/exemple/spoonmate/Finance.java
@@ -5,14 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Finance extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Finance.class.getResource("finance.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
-        stage.setTitle("Finances");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Finance.class.getResource("main.fxml"));
+        Scene scene = new Scene(loader.load(), 900, 600);
+        stage.setTitle("Application de Gestion");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/org/exemple/spoonmate/MainController.java
@@ -0,0 +1,34 @@
+package org.exemple.spoonmate;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Node;
+
+import java.io.IOException;
+
+public class MainController {
+
+    @FXML
+    private StackPane contentArea;
+
+    @FXML
+    public void initialize() throws IOException {
+        showFinance();
+    }
+
+    @FXML
+    public void showFinance() throws IOException {
+        loadPage("finance.fxml");
+    }
+
+    @FXML
+    public void showDashboard() throws IOException {
+        loadPage("dashboard.fxml");
+    }
+
+    private void loadPage(String fxml) throws IOException {
+        Node page = FXMLLoader.load(getClass().getResource(fxml));
+        contentArea.getChildren().setAll(page);
+    }
+}
