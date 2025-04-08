fe449a13ef96b0c3674add3100bc54dffea58882
Phoschi <phoschimere@gmail.com>
2025-04-08 14:24:15 +0200

    page finance V01 terminé

--- src/main/java/org/exemple/spoonmate/Finance.java
+++ src/main/java/org/exemple/spoonmate/Finance.java
@@ -11,8 +11,8 @@ public class Finance extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Finance.class.getResource("finance.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        stage.setTitle("Finances");
         stage.setScene(scene);
         stage.show();
     }
