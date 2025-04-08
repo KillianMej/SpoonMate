d8924c5d02e20b52584355cf95e4e62f4cfe9ea6
Phoschi <phoschimere@gmail.com>
2025-04-08 14:05:22 +0200

    first commit

--- src/main/java/org/exemple/spoonmate/FinanceController.java
+++ src/main/java/org/exemple/spoonmate/FinanceController.java
@@ -3,7 +3,7 @@ package org.exemple.spoonmate;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class FinanceController {
     @FXML
     private Label welcomeText;
 
